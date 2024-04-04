8e96858d74a3f87a3825b1c095b40dd26c69ef10
Lycoris314 <159458009+Lycoris314@users.noreply.github.com>
2024-04-04 23:58:00 +0900

    VSCodeからコミット

--- 2048.js
+++ 2048.js
@@ -1,24 +1,23 @@
 class GameField {
-
     #field;
     #jqRoot;
 
     constructor(jqRoot) {
-
         this.#jqRoot = jqRoot;
 
-        this.#field = new Array(4).fill(null)
-            .map(_ => new Array(4).fill(null))
+        this.#field = new Array(4)
+            .fill(null)
+            .map((_) => new Array(4).fill(null));
 
         this.putPanel();
         this.putPanel();
     }
 
     update(dy, dx) {
-
         //移動の結果をこれに入れていく
-        let outcome = new Array(4).fill(null)
-            .map(_ => new Array(4).fill(null))
+        let outcome = new Array(4)
+            .fill(null)
+            .map((_) => new Array(4).fill(null));
 
         //衝突して消滅するパネルと最後の移動距離を入れていく
         let del = [];
@@ -30,19 +29,18 @@ class GameField {
                 if (panel !== null) {
                     eachUpdate(dy, dx, y, x, panel, this.field);
                 }
-            })
-        })
+            });
+        });
 
         //どのパネルも移動しなかった場合はセルを追加しない。
-        if (R.equals(this.field, outcome)) return { score: 0, inAnimation: false };
+        if (R.equals(this.field, outcome))
+            return { score: 0, inAnimation: false };
 
-
-        del.forEach(elm => {
+        del.forEach((elm) => {
             elm.panel.beforeUnion(dy, dx, elm.moveLength);
-        })
+        });
 
-        grow.forEach(elm => elm.union());
-
+        grow.forEach((elm) => elm.union());
 
         this.#field = outcome;
         outcome.forEach((arr, y) => {
@@ -50,60 +48,63 @@ class GameField {
                 if (elm !== null) {
                     elm.slide(y, x);
                 }
-            })
-        })
+            });
+        });
 
         this.putPanel();
 
-
-        const score = R.sum(grow.map(elm => Math.pow(2, elm.num + 1)));
+        const score = R.sum(grow.map((elm) => Math.pow(2, elm.num + 1)));
         return { score: score, inAnimation: true };
 
-
         function eachUpdate(dy, dx, y, x, panel, field) {
-
             //移動方向に向かってフィールド境界までの距離
             const dist =
-                dy === 1 ? 3 - y :
-                    dy === -1 ? y :
-                        dx === 1 ? 3 - x :
-                            dx === -1 ? x : null;
+                dy === 1
+                    ? 3 - y
+                    : dy === -1
+                    ? y
+                    : dx === 1
+                    ? 3 - x
+                    : dx === -1
+                    ? x
+                    : null;
 
             //移動方向にあるパネルたち(空=nullを含む)
             const path = (() => {
                 let panels = [];
 
                 for (let i = 1; i <= dist; i++) {
-                    panels.push(field[y + i * dy][x + i * dx])
+                    panels.push(field[y + i * dy][x + i * dx]);
                 }
                 return panels;
-            })()
+            })();
 
             //pathからnullを抜いたもの
-            const nullRemovedPath = path.filter(elm => elm !== null);
+            const nullRemovedPath = path.filter((elm) => elm !== null);
 
-            const nRPtoNum = nullRemovedPath.map(e => e.num);
+            const nRPtoNum = nullRemovedPath.map((e) => e.num);
 
             //移動先で合体して消滅する条件
             const disapCond = ((arr, n) => {
                 if (arr.length === 0) return false;
                 if (arr[0] !== n) return false;
                 if (arr.length === 2 && arr[0] === arr[1]) return false;
-                if (arr.length === 3 && arr[0] === arr[1] && arr[1] !== arr[2]) return false;
+                if (arr.length === 3 && arr[0] === arr[1] && arr[1] !== arr[2])
+                    return false;
                 return true;
-            })(nRPtoNum, panel.num)
+            })(nRPtoNum, panel.num);
 
             //pathの途中で他のパネルが合体する場合に1
             const midDisp2 = (arr) => {
                 if (arr.length === 3 && arr[1] === arr[2]) return 1;
                 return 0;
-            }
+            };
 
             //rは移動距離
             if (disapCond) {
                 const r = dist + 1 - nRPtoNum.length + midDisp2(nRPtoNum);
                 del.push({ panel: panel, moveLength: r });
-                grow.push(nullRemovedPath[0])
+                grow.push(nullRemovedPath[0]);
                 return;
             }
 
@@ -116,33 +117,29 @@ class GameField {
                 return 0;
             };
 
-            const r = dist - nRPtoNum.length + midDisp(nRPtoNum)
+            const r = dist - nRPtoNum.length + midDisp(nRPtoNum);
             outcome[y + r * dy][x + r * dx] = field[y][x];
         }
-
-
     }
 
-
     reset() {
-        this.field.flat().forEach(elm => {
+        this.field.flat().forEach((elm) => {
             if (elm !== null) {
                 elm.removeElement();
             }
-        })
+        });
     }
 
     isGameOver() {
-
-        if (this.field.flat().some(elm => elm === null)) return false;
+        if (this.field.flat().some((elm) => elm === null)) return false;
 
         return noMove(this.field);
     }
 
     isGameClear() {
-
-        return this.field.flat()
-            .map(elm => elm === null ? null : elm.num)
+        return this.field
+            .flat()
+            .map((elm) => (elm === null ? null : elm.num))
             .includes(10);
     }
 
@@ -150,60 +147,53 @@ class GameField {
         return this.#field;
     }
 
-
     //空きマスにパネルをランダム追加する。
     putPanel() {
-        R.pipe(
-            emptyCells,
-            randomSelect,
-            this.setPanelTo
-        )(this.#field)
+        R.pipe(emptyCells, randomSelect, this.setPanelTo)(this.#field);
     }
 
     setPanelTo = (yx) => {
-        const panel = new Panel(...yx, randomNum(), this.#jqRoot)
-        this.setField(panel, ...yx)
-    }
+        const panel = new Panel(...yx, randomNum(), this.#jqRoot);
+        this.setField(panel, ...yx);
+    };
 
     setField(panel, y, x) {
         this.#field[y][x] = panel;
     }
 }
 
-
 const randomNum = () => {
-    const r = Math.random()
-    return r < 0.9 ? 0 : 1
-}
+    const r = Math.random();
+    return r < 0.9 ? 0 : 1;
+};
 
 const emptyCells = (matrix) => {
     let emptyCells = [];
     matrix.forEach((arr, y) => {
         arr.forEach((cell, x) => {
             if (cell === null) {
-                emptyCells.push([y, x])
+                emptyCells.push([y, x]);
             }
-        })
-    })
+        });
+    });
     return emptyCells;
-}
+};
 
 const randomSelect = (arr) => {
     const index = Math.floor(Math.random() * arr.length);
-    return arr[index]
-}
+    return arr[index];
+};
 
 //隣接ペアがすべて異なるかどうか。
 const noMove = (matrix) => {
-
     function f(mat) {
         for (let arr of mat) {
-            const row = arr.map(elm => elm.num);
+            const row = arr.map((elm) => elm.num);
 
-            if (row[0] === row[1] || row[1] === row[2] || row[2] === row[3]) return false;
+            if (row[0] === row[1] || row[1] === row[2] || row[2] === row[3])
+                return false;
         }
         return true;
     }
-    return f(matrix) && f(R.transpose(matrix))
-}
-
+    return f(matrix) && f(R.transpose(matrix));
+};
--- main.js
+++ main.js
@@ -1,133 +1,133 @@
 $(() => {
-  //パラメータ
-  const p = {
-    stopKeyEvent: false, //ゲームオーバー画面など表示中
-    ruleShowing: false, //ルール画面表示中
-    isCleared: false,
-    inAnimation: false,
-    score: 0,
-    highScore: Number(localStorage.getItem("highScore")),
-  };
+    //パラメータ
+    const p = {
+        stopKeyEvent: false, //ゲームオーバー画面など表示中
+        ruleShowing: false, //ルール画面表示中
+        isCleared: false,
+        inAnimation: false,
+        score: 0,
+        highScore: Number(localStorage.getItem("highScore")),
+    };
 
-  const MOVE_TIME = 300; //パネルの移動時間(ms)
+    const MOVE_TIME = 300; //パネルの移動時間(ms)
 
-  let gameField = new GameField($("main"));
-
-  $(".score.high>.num").text(p.highScore === null ? 0 : p.highScore);
-
-  //十字キー
-  $("html").on("keydown", (e) => {
-    if (p.stopKeyEvent || p.ruleShowing || p.inAnimation) return;
-
-    let r;
-    switch (e.key) {
-      case "ArrowUp":
-      case "w":
-        r = gameField.update(-1, 0);
-        break;
-      case "ArrowDown":
-      case "s":
-        r = gameField.update(1, 0);
-        break;
-      case "ArrowLeft":
-      case "a":
-        r = gameField.update(0, -1);
-        break;
-      case "ArrowRight":
-      case "d":
-        r = gameField.update(0, 1);
-        break;
-      default:
-        return;
-    }
-
-    p.score += r.score;
-    $(".score.now >.num").text(p.score);
-
-    //アニメーションが生じるならその間イベントの受付を停止する。
-    if (r.inAnimation) {
-      p.inAnimation = true;
-      setTimeout(() => {
-        p.inAnimation = false;
-      }, MOVE_TIME);
-    }
-
-    //ゲームクリア
-    if (gameField.isGameClear() && !p.isCleared) {
-      p.stopKeyEvent = true;
-      p.isCleared = true;
-
-      $("div.gameClear").addClass("show");
-      $("span.score").text(p.score);
-
-      //記録塗り替え
-      if (p.highScore === null || p.score > p.highScore) {
-        p.highScore = p.score;
-        localStorage.setItem("highScore", p.score);
-      }
-    }
-    //ゲームオーバー
-    else if (gameField.isGameOver()) {
-      doWhenGameover();
-    }
-  });
-
-  function doWhenGameover() {
-    p.stopKeyEvent = true;
-
-    $("div.gameOver").addClass("show");
-    $("span.score").text(p.score);
-
-    if (p.highScore === null || p.score > p.highScore) {
-      p.highScore = p.score;
-      localStorage.setItem("highScore", p.score);
-    }
-  }
-
-  //リスタートボタン
-  $("button.restart").on("click", () => {
-    p.score = 0;
-    $(".score.now >.num").text(0);
-
-    gameField.reset();
-    gameField = new GameField($("main"));
-
-    $("div.gameOver").removeClass("show");
-    $("div.gameClear").removeClass("show");
+    let gameField = new GameField($("main"));
 
     $(".score.high>.num").text(p.highScore === null ? 0 : p.highScore);
 
-    p.isCleared = false;
-    p.stopKeyEvent = false;
-  });
+    //十字キー
+    $("html").on("keydown", (e) => {
+        if (p.stopKeyEvent || p.ruleShowing || p.inAnimation) return;
 
-  //続けるボタン
-  $("button.continue").on("click", () => {
-    $("div.gameClear").removeClass("show");
+        let r;
+        switch (e.key) {
+            case "ArrowUp":
+            case "w":
+                r = gameField.update(-1, 0);
+                break;
+            case "ArrowDown":
+            case "s":
+                r = gameField.update(1, 0);
+                break;
+            case "ArrowLeft":
+            case "a":
+                r = gameField.update(0, -1);
+                break;
+            case "ArrowRight":
+            case "d":
+                r = gameField.update(0, 1);
+                break;
+            default:
+                return;
+        }
 
-    p.stopKeyEvent = false;
+        p.score += r.score;
+        $(".score.now >.num").text(p.score);
 
-    //ゲームクリア→続ける→即ゲームオーバー
-    if (gameField.isGameOver()) {
-      doWhenGameover();
+        //アニメーションが生じるならその間イベントの受付を停止する。
+        if (r.inAnimation) {
+            p.inAnimation = true;
+            setTimeout(() => {
+                p.inAnimation = false;
+            }, MOVE_TIME);
+        }
+
+        //ゲームクリア
+        if (gameField.isGameClear() && !p.isCleared) {
+            p.stopKeyEvent = true;
+            p.isCleared = true;
+
+            $("div.gameClear").addClass("show");
+            $("span.score").text(p.score);
+
+            //記録塗り替え
+            if (p.highScore === null || p.score > p.highScore) {
+                p.highScore = p.score;
+                localStorage.setItem("highScore", p.score);
+            }
+        }
+        //ゲームオーバー
+        else if (gameField.isGameOver()) {
+            doWhenGameover();
+        }
+    });
+
+    function doWhenGameover() {
+        p.stopKeyEvent = true;
+
+        $("div.gameOver").addClass("show");
+        $("span.score").text(p.score);
+
+        if (p.highScore === null || p.score > p.highScore) {
+            p.highScore = p.score;
+            localStorage.setItem("highScore", p.score);
+        }
     }
-  });
 
-  //ルールボタン
-  $("button.rule, div.ruleBack").on("click", (_) => {
-    $("div.ruleBack").toggleClass("show");
-    p.ruleShowing = !p.ruleShowing;
-  });
+    //リスタートボタン
+    $("button.restart").on("click", () => {
+        p.score = 0;
+        $(".score.now >.num").text(0);
 
-  $("div.rulePage").on("click", (e) => {
-    e.stopPropagation();
-  });
+        gameField.reset();
+        gameField = new GameField($("main"));
 
-  //デバッグ用
-  // $("html").on("keydown", (e) => {
-  //     if (e.key == "p") {
-  //         console.log(p);
-  //         console.log(gameField.field);
-  //     }
-  //})
+        $("div.gameOver").removeClass("show");
+        $("div.gameClear").removeClass("show");
+
+        $(".score.high>.num").text(p.highScore === null ? 0 : p.highScore);
+
+        p.isCleared = false;
+        p.stopKeyEvent = false;
+    });
+
+    //続けるボタン
+    $("button.continue").on("click", () => {
+        $("div.gameClear").removeClass("show");
+
+        p.stopKeyEvent = false;
+
+        //ゲームクリア→続ける→即ゲームオーバー
+        if (gameField.isGameOver()) {
+            doWhenGameover();
+        }
+    });
+
+    //ルールボタン
+    $("button.rule, div.ruleBack").on("click", (_) => {
+        $("div.ruleBack").toggleClass("show");
+        p.ruleShowing = !p.ruleShowing;
+    });
+
+    $("div.rulePage").on("click", (e) => {
+        e.stopPropagation();
+    });
+
+    //デバッグ用
+    // $("html").on("keydown", (e) => {
+    //     if (e.key == "p") {
+    //         console.log(p);
+    //         console.log(gameField.field);
+    //     }
+    //})
 });
